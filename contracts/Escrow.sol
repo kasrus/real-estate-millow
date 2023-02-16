de7a5e4aa3aa20a1a94cf97d0aca586bbcdb5747
kasrus <kruslim@gmail.com>
2023-02-15 20:13:21 -0800

    List Property

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -15,12 +15,41 @@ contract Escrow {
     address payable public seller;
     address public inspector;
 
-    constuctor(address _nftAddress, address payable _seller, 
+    mapping(uint => bool) public isListed;
+    mapping(uint => uint) public purchasePrice;
+    mapping(uint => uint) public escrowAmount;
+    mapping(uint => address) public buyer;
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call this method");
+        _;
+    }
+
+    modifier onlyBuyer(uint _nftID) {
+        require(msg.sender == buyer[_nftID], "Only buyer can call this method");
+        _;
+    }
+
+    constructor(address _nftAddress, address payable _seller, 
                 address _inspector, address _lender) {
         nftAddress = _nftAddress;
         seller = _seller;
         inspector = _inspector;
         lender = _lender;
     }
-    
+
+    function list(uint _nftID, address _buyer, 
+        uint _purchasePrice, uint _escrowAmount) public payable onlySeller {
+        // Transfer NFT from seller to this smart contract
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        buyer[_nftID] = _buyer;
+        escrowAmount[_nftID] = _escrowAmount;
+    }
+
+    // Put under contract (only buyer - payable escrow)
+    function depositEarnest(uint _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID]);
+    }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -6,14 +6,79 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
-    let realEstate;
+    let realEstate, escrow;
+    let seller, buyer, inspector, lender;
+    let transaction;
 
     beforeEach(async()  => {
-        //Deploy the real estate smart contract
-        const RealEstate = await ethers.getContractFactory('Real Estate');
-        realEstate = await RealEstate.deploy();
-    }) 
-    it('saves the addresses', async() => {
+        // Get all the addresses required
+        [buyer, seller, inspector, lender] = await ethers.getSigners();
 
+        // Deploy the real estate smart contract
+        const RealEstate = await ethers.getContractFactory('RealEstate');
+        realEstate = await RealEstate.deploy();
+        
+        // Mint 
+        transaction = await realEstate.connect(seller)
+            .mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS");
+        await transaction.wait();
+
+        // Deploy escrow
+        const Escrow = await ethers.getContractFactory("Escrow");
+        escrow = await Escrow.deploy(realEstate.address, seller.address, inspector.address, lender.address);
+    
+        // Approve property
+        transaction = await realEstate.connect(seller).approve(escrow.address, 1);
+        await transaction.wait();
+
+        // List property
+        transaction = await escrow.connect(seller).list(1, buyer.address,
+                tokens(10), tokens(5));
+        await transaction.wait();
+    }) 
+
+    describe('Deployment',  () => {
+        it('returns NFT address', async() => {
+            const result = await escrow.nftAddress();
+            expect(result).to.equal(realEstate.address);
+        })
+        it('returns seller', async() => {
+            const result = await escrow.seller();
+            expect(result).to.equal(seller.address);
+         })
+         it('returns inspector', async() => {
+            const result = await escrow.inspector();
+            expect(result).to.equal(inspector.address);
+         })
+         it('returns lender', async() => {
+            const result = await escrow.lender();
+            expect(result).to.equal(lender.address);
+         })  
+    })
+    describe('Listing', () => {
+        describe('Success', () => {
+            it('updates as listed', async() => {
+                expect(await escrow.isListed(1)).to.equal(true);
+            })
+            it('updates ownership', async() => {
+                expect(await realEstate.ownerOf(1)).to.equal(escrow.address);
+            })
+            it ('returns buyer', async() => {
+                expect(await escrow.buyer(1)).to.equal(buyer.address);
+            })
+            it('returns purchase price', async() => {
+                expect(await escrow.purchasePrice(1)).to.equal(tokens(10));
+            })
+            it('returns escrow amount', async() => {
+                expect(await escrow.escrowAmount(1)).to.equal(tokens(5));
+            })
+        })
+        describe('Failure', () => {
+            it('rejects anyone other than the seller', async() => {
+                await expect(escrow.connect(buyer).list(1, buyer.address, tokens(10), tokens(5)))
+                    .to.be.reverted;
+            })
+        })
+        
     })
 })
